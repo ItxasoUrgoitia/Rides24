3838f395bb18b0aff04200fcde1a5b929d57d1b6
eneekoruiz <eruiz084@ikasle.ehu.eus>
2025-10-03 13:13:58 +0200

    IssueS

--- src/main/java/dataAccess/ObjectdbManagerServer.java
+++ src/main/java/dataAccess/ObjectdbManagerServer.java
@@ -42,7 +42,7 @@ public class ObjectdbManagerServer extends JDialog {
 		try {
 			
 			ObjectdbManagerServer dialog = new ObjectdbManagerServer();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
